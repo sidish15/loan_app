382157b7628d8367f6d4e71c8ad8ec9e52f7aaff
siddharth <110058016+sidish15@users.noreply.github.com>
2024-05-23 18:45:00 +0530

    server done

--- server/routes/loan.js
+++ server/routes/loan.js
@@ -1,11 +1,14 @@
 const express=require("express")
 const router=express.Router();
 
-router.post("/createLoan",createLoan)
-router.get("/allLoans",getAllLoans);
-router.get("/loans/:id",getLoansById);
-router.get("/payments/:id",getPaymentsById);
-router.post("/doPayment",doPayment);
-router.put("/update",updateState)
+const authentication=require("../middleware/authentication");
+const { createLoan, getAllLoans, getLoansById, getPaymentsById, doPayment, updateState } = require("../controllers/loan");
+
+router.post("/createLoan",authentication,createLoan)
+router.get("/allLoans",authentication,getAllLoans);
+router.get("/loans/:id",authentication,getLoansById);
+router.get("/payments/:id",authentication,getPaymentsById);
+router.post("/doPayment",authentication,doPayment);
+router.put("/update",authentication,updateState)
 
 module.exports=router
