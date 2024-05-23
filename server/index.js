d9ae78f5d1ba9fc30e547bcc864dac46c06cd0f9
siddharth <110058016+sidish15@users.noreply.github.com>
2024-05-23 13:10:10 +0530

    added:loan routes

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use("*", cors());
 
 app.use("/api/user",require("./routes/user"))
+app.use("/api/loan",require("./routes/loan"))
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
--- server/routes/loan.js
+++ server/routes/loan.js
@@ -1,3 +1,11 @@
 const express=require("express")
 const router=express.Router();
 
+router.post("/createLoan",createLoan)
+router.get("/allLoans",getAllLoans);
+router.get("/loans/:id",getLoansById);
+router.get("/payments/:id",getPaymentsById);
+router.post("/doPayment",doPayment);
+router.put("/update",updateState)
+
+module.exports=router
